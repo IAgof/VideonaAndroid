f8e6b016894a111e4030234709494eeb93a2f88e
Juan Javier Cabanas <javiCabanas@videona.com>
2015-02-13 13:59:12 +0100

    -tracked all login buttons

--- app/src/main/java/com/videonasocialmedia/videona/LoginActivity.java
+++ app/src/main/java/com/videonasocialmedia/videona/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -58,12 +59,11 @@ public class LoginActivity extends Activity {
                 .getSharedPreferences("USER_INFO", MODE_PRIVATE);
 
 
-        t =app.getTracker();
+        t = app.getTracker();
 
-        t.setScreenName("LoginActivity");
-        t.send(new HitBuilders.AppViewBuilder().build());
+        //t.setScreenName("LoginActivity");
+        //t.send(new HitBuilders.AppViewBuilder().build());
     }
-
 
 
     /**
@@ -74,18 +74,47 @@ public class LoginActivity extends Activity {
         startActivity(new Intent(getApplicationContext(), UserSignUpActivity.class));
     }
 
+    private void sendButtonTracked(int id) {
+        String label;
+        switch (id) {
+            case R.id.send_login_button:
+                label = "login with google";
+                break;
+            case R.id.login_facebook_button:
+                label = "login with google";
+                break;
+            case R.id.login_g_plus_button:
+                label = "login with google";
+                break;
+            case R.id.login_twitter_button:
+                label = "Login with twitter";
+                break;
+            case R.id.new_user_button:
+                label = "Create new User";
+                break;
+            default:
+                label = "other";
+        }
+        t.send(new HitBuilders.EventBuilder()
+                .setCategory("Login")
+                .setAction("button clicked")
+                .setLabel(label)
+                .build());
+        GoogleAnalytics.getInstance(app.getBaseContext()).dispatchLocalHits();
+    }
+
     /**
      * Try to login the user using the credentials provided by userTextField and passwordTextField
      */
     @OnClick(R.id.send_login_button)
-    public void login() {
+    public void login(View v) {
+        sendButtonTracked(v.getId());
         t.send(new HitBuilders.EventBuilder()
                 .setCategory("Categoría")
                 .setAction("Acción")
                 .setLabel("Etiqueta")
                 .build());
 
-        GoogleAnalytics.getInstance(app.getBaseContext()).dispatchLocalHits();
 
         //TODO remove next line when remember-me is working and uncomment the rest of the method
         //startActivity(new Intent(getApplicationContext(), RecordActivity.class));
