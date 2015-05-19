8592b9f0adfe285c5fad5359483bbab0a096786a
amm_videona <a.martinez@videona.com>
2015-05-19 11:48:57 +0200

    - LoadingProjectUseCaseTest done, success.

--- app/src/androidTest/java/com/videonasocialmedia/videona/domain/initapp/LoadingProjectUseCaseTest.java
+++ app/src/androidTest/java/com/videonasocialmedia/videona/domain/initapp/LoadingProjectUseCaseTest.java
@@ -7,34 +7,53 @@
 
 package com.videonasocialmedia.videona.domain.initapp;
 
-import android.test.AndroidTestCase;
+import android.content.Context;
+import android.test.ActivityInstrumentationTestCase2;
 
 import com.videonasocialmedia.videona.presentation.mvp.presenters.OnInitAppEventListener;
+import com.videonasocialmedia.videona.presentation.views.activity.InitAppActivity;
 
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-public class LoadingProjectUseCaseTest extends AndroidTestCase {
+public class LoadingProjectUseCaseTest extends ActivityInstrumentationTestCase2<InitAppActivity> {
 
     LoadingProjectUseCase useCase;
 
     OnInitAppEventListener listener;
 
+    InitAppActivity activity;
+
+    Context context;
+
+
     public LoadingProjectUseCaseTest(){
-
-        useCase = new LoadingProjectUseCase(getContext());
-
-        listener = mock(OnInitAppEventListener.class);
+        super(InitAppActivity.class);
 
     }
 
+
+
     public void testStartNewProject(){
+
+        activity = getActivity();
+
+        assertNotNull(activity);
+
+        context = activity.getApplicationContext();
+
+        assertNotNull(context);
+
+        useCase = new LoadingProjectUseCase(context);
+
+        listener = mock(OnInitAppEventListener.class);
 
         useCase.checkProjectState(listener);
 
         verify(listener, atLeastOnce()).onLoadingProjectSuccess();
 
     }
+
 
 }
