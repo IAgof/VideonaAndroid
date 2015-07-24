3b0ac32dcf954cd6b42948206018e60a589c216f
Verónica Lago <verofomi@gmail.com>
2015-07-24 14:12:12 +0200

    Optimized obtain videos from memory card use case

--- app/src/main/java/com/videonasocialmedia/videona/domain/ObtainLocalVideosUseCase.java
+++ app/src/main/java/com/videonasocialmedia/videona/domain/ObtainLocalVideosUseCase.java
@@ -7,6 +7,8 @@ import com.videonasocialmedia.videona.utils.Constants;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by jca on 20/5/15.
@@ -33,38 +35,26 @@ public class ObtainLocalVideosUseCase {
         }
     }
 
-    class Pair implements Comparable {
-        public long t;
-        public File f;
-
-        public Pair(File file) {
-            f = file;
-            t = file.lastModified();
-        }
-
-        public int compareTo(Object o) {
-            long u = ((Pair) o).t;
-            return t < u ? -1 : t == u ? 0 : 1;
-        }
-    }
-
     private ArrayList<Video> obtainVideosFromPath(String path) {
         ArrayList<Video> videos = null;
-        File f = new File(path);
-        File file[] = f.listFiles();
+        File directory = new File(path);
+        File[] files = directory.listFiles();
 
-        if (file != null && file.length > 0) {
+        if (files != null && files.length > 0) {
             videos = new ArrayList<>();
-            Pair[] pairs = new Pair[file.length];
-            for (int i = 0; i < file.length; i++)
-                pairs[i] = new Pair(file[i]);
 
-            Arrays.sort(pairs);
-            for (int i = file.length - 1; i >= 0; i--) {
-                file[i] = pairs[i].f;
-                if (file[i].getName().endsWith(".mp4") && file[i].isFile())
-                    videos.add(new Video(path + File.separator + file[i].getName(), 0, 0));
+            Collections.sort(Arrays.asList(files), new Comparator<File>() {
+                public int compare(File f1, File f2) {
+                    long d1 = f1.lastModified();
+                    long d2 = f2.lastModified();
+                    return d1 > d2 ? 1 : d1 < d2 ? -1 : 0;
+                }
+            });
+            for (int i = files.length - 1; i >= 0; i--) {
+                if (files[i].getName().endsWith(".mp4") && files[i].isFile())
+                    videos.add(new Video(path + File.separator + files[i].getName(), 0, 0));
             }
+
         }
 
         return videos;
