d57a1c982581a414f69d75b893cb06d885a615d4
Juan Javier Cabanas <jca@MacBook-Pro-de-Iago.local>
2015-07-28 14:01:27 +0200

    started refactor to integrate eventbus

---
+++ app/src/main/java/com/videonasocialmedia/videona/eventbus/events/videosretrieved/VideosRetrievedEvent.java
@@ -0,0 +1,22 @@
+package com.videonasocialmedia.videona.eventbus.events.videosretrieved;
+
+import android.support.annotation.Nullable;
+
+import com.videonasocialmedia.videona.model.entities.editor.media.Video;
+
+import java.util.List;
+
+/**
+ * Created by jca on 28/7/15.
+ */
+public abstract class VideosRetrievedEvent {
+    public final List<Video> videoList;
+
+    /**
+     * Creates a VideosRetrievedEvent
+     * @param videoList the retrieved video list. Null if not retrieved or empty
+     */
+    protected VideosRetrievedEvent(@Nullable List<Video> videoList){
+        this.videoList=videoList;
+    }
+}
