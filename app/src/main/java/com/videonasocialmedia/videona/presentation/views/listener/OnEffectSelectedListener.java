d8b274cf54d7dc9d0bff115d966b054a8f5928ef
Verónica Lago <verofomi@gmail.com>
2015-11-03 11:38:49 +0100

    Added new filter icons in record activity. Refactored adapter and view holder of filter. Created new general effect class. Changed filter button position in record activity.

---
+++ app/src/main/java/com/videonasocialmedia/videona/presentation/views/listener/OnEffectSelectedListener.java
@@ -0,0 +1,13 @@
+package com.videonasocialmedia.videona.presentation.views.listener;
+
+import com.videonasocialmedia.videona.presentation.views.adapter.Effect;
+
+/**
+ * Created by Veronica Lago Fominaya on 03/11/2015.
+ */
+public interface OnEffectSelectedListener {
+    /**
+     * @param effect
+     */
+    void onEffectSelected(Effect effect);
+}
