95e239a730466b9f65d1f300fa2948752c458c20
Verónica Lago <verofomi@gmail.com>
2015-11-24 16:47:29 +0100

    Added toolbar in settings activity with custom style.

--- app/src/alpha/java/com/videonasocialmedia/videona/presentation/views/activity/SettingsActivity.java
+++ app/src/alpha/java/com/videonasocialmedia/videona/presentation/views/activity/SettingsActivity.java
@@ -11,8 +11,10 @@
 package com.videonasocialmedia.videona.presentation.views.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.qordoba.sdk.Qordoba;
+import com.videonasocialmedia.videona.R;
 import com.videonasocialmedia.videona.presentation.views.fragment.SettingsFragment;
 
 /**
@@ -23,11 +25,15 @@ public class SettingsActivity extends VideonaActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // Display the fragment as the main content.
-        Qordoba.setCurrentNavigationRoute(android.R.id.content,this.getClass().getName());
+        Qordoba.setCurrentNavigationRoute(android.R.id.content, this.getClass().getName());
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.fragment_preferences, new SettingsFragment())
                 .commit();
     }
 
