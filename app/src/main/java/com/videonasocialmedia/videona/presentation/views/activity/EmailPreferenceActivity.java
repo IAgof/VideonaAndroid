31b74a30292f35e8ed0bad6aef9b0017ee9df922
Verónica Lago <verofomi@gmail.com>
2015-11-26 17:59:40 +0100

    Added user account fields.

--- app/src/main/java/com/videonasocialmedia/videona/presentation/views/activity/EmailPreferenceActivity.java
+++ app/src/main/java/com/videonasocialmedia/videona/presentation/views/activity/EmailPreferenceActivity.java
@@ -6,40 +6,25 @@ import android.support.v7.widget.Toolbar;
 
 import com.qordoba.sdk.Qordoba;
 import com.videonasocialmedia.videona.R;
-import com.videonasocialmedia.videona.presentation.views.fragment.SettingsFragment;
 
 /**
  * Created by Veronica Lago Fominaya on 26/11/2015.
  */
-public class SettingsBaseActivity extends VideonaActivity {
+public class EmailPreferenceActivity extends VideonaActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_settings_user_account);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.emailPreference);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
 
         // Display the fragment as the main content.
         Qordoba.setCurrentNavigationRoute(android.R.id.content, this.getClass().getName());
-        getFragmentManager().beginTransaction()
-                .replace(R.id.fragment_preferences, new SettingsFragment())
-                .commit();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mixpanel.timeEvent("Time in Settings Activity");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mixpanel.track("Time in Settings Activity");
     }
 
 }
