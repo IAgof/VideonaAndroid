5e466682fc738b384883a4e52cd3f22f25c46c35
amm_videona <a.martinez@videona.com>
2015-12-22 16:07:55 +0100

    Added new list to EffectProvider. ShaderEffectList, join distorsion and color effects.

--- app/src/alpha/java/com/videonasocialmedia/videona/presentation/views/fragment/SettingsFragment.java
+++ app/src/alpha/java/com/videonasocialmedia/videona/presentation/views/fragment/SettingsFragment.java
@@ -10,7 +10,39 @@
 
 package com.videonasocialmedia.videona.presentation.views.fragment;
 
+import android.os.Bundle;
+import android.preference.Preference;
+
 /**
  * This class is used to manage the setting menu.
  */
-public class SettingsFragment extends SettingsBaseFragment {}
+public class SettingsFragment extends SettingsBaseFragment {
+
+    private Preference joinBetaPref;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        joinBetaPref = findPreference("leave_beta");
+        joinBetaPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                new LeaveBetaDialogFragment().show(getFragmentManager(), "leaveBetaDialogFragment");
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sharedPreferences.registerOnSharedPreferenceChangeListener(preferencesPresenter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sharedPreferences.registerOnSharedPreferenceChangeListener(preferencesPresenter);
+    }
+}
