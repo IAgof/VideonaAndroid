910f4f4389a8f50f279ab9e5661fae299ce4aaa7
Verónica Lago <verofomi@gmail.com>
2016-02-02 15:50:31 +0100

    Fixed bug in preview gallery activity. Fixed bug in manifest in share video activity name.

--- app/src/alpha/java/com/videonasocialmedia/videona/presentation/views/activity/VideoFullScreenPreviewActivity.java
+++ app/src/alpha/java/com/videonasocialmedia/videona/presentation/views/activity/VideoFullScreenPreviewActivity.java
@@ -46,8 +46,8 @@ public class VideoFullScreenPreviewActivity extends VideonaActivity implements O
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_preview);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
