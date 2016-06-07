719215e4fe31ae0e62ef4298ccfef01ebc6c9f08
Juan Javier Cabanas <jca@MacBook-Pro-de-Iago.local>
2016-06-07 12:08:48 +0200

    fix some bugs on layouts. added author and title of birthday music.

--- app/src/alpha/java/com/videonasocialmedia/videona/model/sources/MusicSource.java
+++ app/src/alpha/java/com/videonasocialmedia/videona/model/sources/MusicSource.java
@@ -30,7 +30,7 @@ public class MusicSource {
         localMusic.add(new Music(R.drawable.imagebutton_music_background_ambient, "Impact Prelude", R.raw.audio_ambiental, R.color.ambient, "Kevin Macleod"));
         localMusic.add(new Music(R.drawable.imagebutton_music_background_jazz, "Monkeys Spinning Monkeys", R.raw.audio_clasica_flauta, R.color.jazz, "Kevin Macleod"));
         localMusic.add(new Music(R.drawable.imagebutton_music_background_folk, "Don't Close Your Eyes", R.raw.audio_folk, R.color.folk, "Josh Woodward"));
-        localMusic.add(new Music(R.drawable.imagebutton_music_background_birthday, "birthday", R.raw.audio_birthday, R.color.birthday, "author"));
+        localMusic.add(new Music(R.drawable.imagebutton_music_background_birthday, "Super Psyched for Your Birthday", R.raw.audio_birthday, R.color.birthday, "The Danimals"));
         localMusic.add(new Music(R.drawable.imagebutton_music_background_hiphop, "I Dunno", R.raw.audio_hiphop, R.color.hiphop, "Grapes"));
         localMusic.add(new Music(R.drawable.imagebutton_music_background_classic, "The Last Slice of Pecan Pie", R.raw.audio_clasica_piano, R.color.classic, "Josh Woodward"));
     }
